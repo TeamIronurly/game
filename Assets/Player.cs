1796ca22c8efc8ee868a316692930bd69d1ce444
Dmitri <codedintellect@gmail.com>
2023-04-11 09:54:40 -0500

    Code simplification Removed redundancy (Character object not necessary, since script is attached to said character)

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,11 +5,9 @@ using System;
 
 public class Player : MonoBehaviour
 {
-    public GameObject Character;
     Rigidbody rb;
 
     public float speed;
-    private float k;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +15,10 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        k = Input.GetAxisRaw("Horizontal");
-    }
-
     void FixedUpdate()
     {
-        if (Math.Abs(k) == 1)
+        float k = Input.GetAxisRaw("Horizontal");
+        if (k != 0)
         {
             rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(k * speed, 0f, 0f), 0.1f);
         }
