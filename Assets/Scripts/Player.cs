8dac8302f0c43d70bfd52556e6649f2df1d1356f
Ironurly <130310941+Ironurly@users.noreply.github.com>
2023-04-12 17:54:41 +0300

    Added jumping mechanics
    * Added jumping mechanics
    
    * Added air jumping mechanics
    
    * "W" key is now an alt key for jumping
    
    * "Player.cs" moved to scripts folder
    
    * Rewrote from the ground up the original movement system
    
    ---------
    
    Co-authored-by: Dmitri <codedintellect@gmail.com>

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [Header("Movement")]
+    public float speed;
+    public LayerMask groundMask;
+
+    [Range(1, 3)]
+    public int Jumps;
+    public float jumpHeight; // How high can the player jump in units
+    private int jumpCnt;
+    private bool jumpTrigger = false;
+    private float g = 9.81f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        jumpTrigger = jumpTrigger || Input.GetButtonDown("Jump");
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 v = rb.velocity;
+
+        if (Physics.CheckSphere(transform.position - Vector3.up, 0.1f, groundMask)) // Plr is touching ground => jumps resets
+        {
+            jumpCnt = Jumps;
+        }
+
+        float k = Input.GetAxisRaw("Horizontal");
+        if (k != 0)
+        {
+            v.x += (k * speed - v.x) * 0.1f;
+        }
+        else
+        {
+            v.x *= 0.95f; // Horizontal drag
+        }
+
+        if (jumpTrigger && jumpCnt > 0)
+        {
+            v.y = Mathf.Sqrt(2 * g * jumpHeight);
+            jumpCnt--;
+        }
+
+        v -= Vector3.up * g * 0.02f; // Gravity
+        rb.velocity = v; // Apply recalculated velocity
+
+        jumpTrigger = false;
+    }
+}
