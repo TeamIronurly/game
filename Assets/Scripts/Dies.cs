0a99e9f13fdf7f2a1acf1dee2abdad08a478bf13
Ironurly <130310941+Ironurly@users.noreply.github.com>
2023-04-16 01:01:24 +0300

    Platform spawning, falling and destruction (#2)
    * Added platforms
    
    * Platforms have falling physics
    
    * Platforms destroy after being submerged in Lava
    
    ---------
    
    Co-authored-by: Artem Arkhipov <arhipartyom@yandex.ru>

---
+++ Assets/Scripts/Dies.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dies : MonoBehaviour
+{
+    private void OnTriggerExit(Collider collision) // if touches "Lava" = dies
+    {
+        if (collision.CompareTag("Lava"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
